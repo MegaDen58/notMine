a08c516dfee31a0000552871a384a489d3a2b547
Denis <samparizona2018@yandex.ru>
2022-05-06 20:53:38 +0300

    added

--- Main.java
+++ Main.java
@@ -1,62 +1,137 @@
 package com.company;
 
-import java.io.*;
-import java.util.Scanner;
+enum ClothesSize{
+    XXS(32){
+        @Override
+        public String getDescription(){
+            return "Детский размер.";
+        }
+    }, XS(34), S(36), M(38), L(40);
+    private final int Eurosize;
 
+    ClothesSize(int i) {
+        Eurosize = i;
+    }
 
-class Function implements Serializable {
-    double x, y;
-
-    double getY() {
-        y = this.x - Math.sin(this.x);
-        return y;
+    public String getDescription(){
+        return "Взрослый размер.";
+    }
+    public String toString(){
+        return name() + " (" + Eurosize + ") " + getDescription();
     }
 }
 
+interface ClothesOfMen{
+    default void dressAMen(){
+        System.out.println("Одеть мужчину.");
+    }
+}
 
-public class Main {
+interface ClothesOfWomen{
+    default void dressAWomen(){
+        System.out.println("Одеть девушку.");
+    }
+}
 
-    public static void main(String[] args) {
-        String text;
-        Scanner in = new Scanner(System.in);
-        Function func = new Function();
-        System.out.print("Введите значения: ");
-        int ss = 1;
-        while (true) {
-            text = in.nextLine();
-            try {
-                double x = Double.parseDouble(text);
-                func.x = x;
-                func.getY();
-                if(ss == 1){
-                    System.out.println("Вы можете ввести одну из следующих комманд: save, upload, check, stop ");
-                    ss++;
-                }
-            } catch (Exception IOex) {
-                if (text.equalsIgnoreCase("save")) {
-                    try (ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream("file.txt"))) {
-                        write.writeObject(func);
-                        System.out.println("Данные сохранены в файл.");
-                    } catch (IOException ex) {
-                        ex.getMessage();
-                    }
-                } else if (text.equalsIgnoreCase("upload")) {
-                    try (ObjectInputStream read = new ObjectInputStream(new FileInputStream("file.txt"))) {
-                        func = (Function) read.readObject();
-                        System.out.println("Всё нормально.");
-                    } catch (Exception ex) {
-                        ex.getMessage();
-                    }
-                } else if (text.equalsIgnoreCase("check")) {
-                    System.out.printf("x: %s\ny: %s\n",func.x, func.y);
-                } else if (text.equalsIgnoreCase("stop")){
-                    break;
-                }
-                else {
-                    System.out.println("Такой команды не существует.");
-                }
-                System.out.println("Вы можете ввести одну из следующих комманд: save, upload, check, stop ");
+abstract class Clothes{
+    private final ClothesSize size;
+    private double price;
+    private final String color;
+
+    Clothes(ClothesSize size, String color, double price){
+        this.size = size;
+        this.price = price;
+        this.color = color;
+    }
+
+    public void setPrice(double x){
+        this.price = x;
+    }
+    public ClothesSize getSize(){
+        return size;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getColor(){
+        return color;
+    }
+}
+
+class Atelier{
+    void dressAMan(Clothes[] clothes){
+        System.out.println("Мужская одежда\n");
+        for(Clothes c : clothes){
+            if(c instanceof ClothesOfMen){
+                System.out.println(c);
             }
         }
+    }
+    void dressAWoman(Clothes[] clothes){
+        System.out.println("Женская одежда\n");
+        for(Clothes c : clothes){
+            if(c instanceof ClothesOfWomen){
+                System.out.println(c);
+            }
+        }
+    }
+    public static class TShirt extends Clothes implements ClothesOfMen, ClothesOfWomen{
+        TShirt(ClothesSize size, String color, double price){
+            super(size, color, price);
+        }
+
+        public String toString() {
+            return "=============" + "\n" + "Размер футболки " + getSize() + "\n" + "Цена: " + getPrice() + "₽" + "\n" + "Цвет: " + getColor();
+        }
+    }
+
+    public static class Trousers extends Clothes implements ClothesOfMen, ClothesOfWomen{
+        Trousers(ClothesSize size, String color, double price){
+            super(size, color, price);
+        }
+
+        public String toString() {
+            return "=============" + "\n" + "Размер штанов " + getSize() + "\n" + "Цена: " + getPrice() + "₽" + "\n" + "Цвет: " + getColor();
+        }
+    }
+
+    public static class Skirt extends Clothes implements ClothesOfWomen{
+        Skirt(ClothesSize size, String color, double price){
+            super(size, color, price);
+        }
+
+        public String toString() {
+            return "=============" + "\n" + "Размер юбки " + getSize() + "\n" + "Цена: " + getPrice() + "₽" + "\n" + "Цвет: " + getColor();
+        }
+    }
+
+    public static class Tie extends Clothes implements ClothesOfMen{
+        Tie(ClothesSize size, String color, double price){
+            super(size, color, price);
+        }
+
+        public String toString() {
+            return "=============" + "\n" + "Размер галстука " + getSize() + "\n" + "Цена: " + getPrice() + "₽" + "\n" + "Цвет: " + getColor();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Clothes[] clothes = {
+                new Atelier.TShirt(ClothesSize.M, "Синяя", 800),
+                new Atelier.TShirt(ClothesSize.S, "Красная", 800),
+                new Atelier.Trousers(ClothesSize.XXS, "Серые", 1300),
+                new Atelier.Trousers(ClothesSize.M, "Чёрные", 1400),
+                new Atelier.Skirt(ClothesSize.XS, "Красная", 2200),
+                new Atelier.Skirt(ClothesSize.L, "Белая", 2500),
+                new Atelier.Tie(ClothesSize.XS, "Чёрный", 800),
+                new Atelier.Tie(ClothesSize.XXS, "Синий", 1200),
+        };
+
+        Atelier atelier = new Atelier();
+        atelier.dressAMan(clothes);
+        System.out.println();
+        atelier.dressAWoman(clothes);
     }
 }
